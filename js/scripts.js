55c3fdd46e57e2e210e9d7a6e1ca2eb70347390c
Cici Harrison and Zoe Zhou <zoe@>
2018-06-20 20:46:30 -0700

    it will replace button type as submit rather than button and will now show a result, but it still adds way to every word.

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,18 @@ $(document).ready(function() {
   $("form#translate").submit(function(event) {
     event.preventDefault();
 
-    debugger;
-
     var phrase = $('input#phrase').val();
     var phraseSplit = phrase.split(" ");
 
-    alert(phrase);
-    alert(phraseSplit);
-
     for (var index = 0; index < phraseSplit.length; index += 1) {
       var word = phraseSplit[index]
-      if ( word[0] === "a" ) {
+      if ( word[0] === "a","e","i","o","u" ) {
         word = word.concat("way");
         phraseSplit[index] = word;
       }
     };
 
-    alert(phraseSplit);
+    console.log(phraseSplit);
 
   });
-
-
 });
