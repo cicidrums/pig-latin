b61d8ba1cb6ee7261f3299743635ce825b7d8497
Cici Harrison and Zoe Zhou <zoe@>
2018-06-20 20:37:48 -0700

    it will add the logic that breaks the user input in an array on individual words and then run a for loop that checks the first letter of each word and concats way

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
-  var result;
+  //var result;
 
-  $("form#phrase").submit(function(event) {
+  $("form#translate").submit(function(event) {
     event.preventDefault();
 
+    debugger;
+
+    var phrase = $('input#phrase').val();
+    var phraseSplit = phrase.split(" ");
+
+    alert(phrase);
+    alert(phraseSplit);
+
+    for (var index = 0; index < phraseSplit.length; index += 1) {
+      var word = phraseSplit[index]
+      if ( word[0] === "a" ) {
+        word = word.concat("way");
+        phraseSplit[index] = word;
+      }
+    };
+
+    alert(phraseSplit);
 
   });
 
